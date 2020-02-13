6fc39ee5ccdbbe0972dcf5faa8cd939f25257f15
MattCrutchley <mcrutchley8@gmail.com>
2020-02-13 14:46:11 +0000

    updated master table

--- slist.sql
+++ slist.sql
@@ -28,7 +28,7 @@ CREATE TABLE `items` (
   `quantity` float NOT NULL,
   `units` varchar(30) NOT NULL,
   PRIMARY KEY (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,6 +50,9 @@ DROP TABLE IF EXISTS `master`;
 CREATE TABLE `master` (
   `user_id` int NOT NULL,
   `item_id` int NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `quantity` float DEFAULT NULL,
+  `units` varchar(50) DEFAULT NULL,
   KEY `user_id` (`user_id`),
   KEY `item_id` (`item_id`),
   CONSTRAINT `master_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
@@ -100,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-13 13:53:05
+-- Dump completed on 2020-02-13 14:45:08
