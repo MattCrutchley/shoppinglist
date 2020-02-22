83ca0522fa2e2749761ebbbcc8ce5d9ab734441d
MattCrutchley <mcrutchley8@gmail.com>
2020-02-22 21:56:17 +0000

    updating routes

--- application/routes.py
+++ application/routes.py
@@ -6,3 +6,11 @@ from application import app
 def home():
 	return render_template('home.html', title='home')
 
+@app.route('/register')
+def register():
+	return render_template('register.html', title='register')
+
+@app.route('/login')
+def login():
+	return render_template('login.html', title='home')
+
