de5be987a70c33eeb1a60147b2da557585779d62
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-02-24 17:05:10 +0000

    added env variables and db object

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask (__name__)
 
-#app.config['SQLALCHEMY_DATABASE_URI']="mysql+pymysql://root:pass123@34.23.35.123/food"
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+
+app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
+
+db = SQLAlchemy(app)
 
 from application import routes
