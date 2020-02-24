d54d930807ddb45e0223efbae8f609d26750f0ac
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-02-24 14:24:03 +0000

    Update create.py

--- create.py
+++ create.py
@@ -1,4 +1,6 @@
 from application import db
-from application.models import Posts
+from application.models import users
+from application.models import items
+from application.models import master
 
 db.create_all()
