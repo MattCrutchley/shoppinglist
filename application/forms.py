a92dbd08f0b7905c4a47c438d8567d963a872fba
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-02-26 14:40:33 +0000

    Update forms.py

--- application/forms.py
+++ application/forms.py
@@ -29,7 +29,7 @@ class additems(FlaskForm):
 
 '''
 class RegistrationForm(FlaskForm):
-    username  = StringField('username',
+    username = StringField('username',
         validators = [
            Required(),
           
