bb7befc8848258653aee81568b23fc034e3f73b4
MattCrutchley <mcrutchley8@gmail.com>
2020-03-01 15:26:31 +0000

    addeed testing

--- tests/db_test.py
+++ tests/db_test.py
@@ -8,23 +8,23 @@ from application.models import users, items, master
 class TestBase(TestCase):
 
     def create_app(self):
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('TEST_DATABASE_URI'))
-    return app
+        config_name = 'testing'
+        app.config.update['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('TEST_DATABASE_URI'))
+        return app
   
     #app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
     
         def setUp(self):
-        db.session.commit()
-        db.drop_all()
-        db.create_all()
+            db.session.commit()
+            db.drop_all()
+            db.create_all()
 
         # create test user
-        test_user = Users(username="Test", password="Test")
+            test_user = Users(username="Test", password="Test")
 
         # save user to database
-        db.session.add(test_user)
-        db.session.commit()
+            db.session.add(test_user)
+            db.session.commit()
 
     def tearDown(self):
 
--- tests/views_test.py
+++ tests/views_test.py
@@ -1,5 +1,15 @@
+from tests.db_test import TestBase
+import unittest
+
+from flask import abort, url_for
+
+from application import app, db
+from application.models import users, items, master
+
+
+
 class TestViews(TestBase):
 
         def test_loginpage_view(self):
-        response = self.client.get(url_for('login'))
-        self.assertEqual(response.status_code, 200)
+            response = self.client.get(url_for('login'))
+            self.assertEqual(response.status_code, 200)
