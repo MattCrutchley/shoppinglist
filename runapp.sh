344a1e340ffbd10f1d6c7c3e63523213cb40aef2
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-03-11 21:00:15 +0000

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -2,6 +2,8 @@
 if [ ! -x "$(command -v docker)" ]; then
 curl https://get.docker.com | sudo bash
 fi
-
+if [[ "$(docker images -q flaskapp 2> /dev/null)" == "" ]]; then
+docker build -t flaskapp .
+fi
 docker run -d -p 5000:5000 --network flaskapp -e DATABASE_URI=mysql+pymysql://root:${rootpass}@mysql:3306/${dbname} -e SECRET_KEY =${SECRET_KEY} --name flaskapp flaskapp
 
